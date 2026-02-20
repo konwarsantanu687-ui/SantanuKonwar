71a827e17a833669b3920c2d3208b8346845af30
konwarsantanu687-ui <konwarsantanu687@gmail.com>
2026-02-20 20:00:52 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,1 +1,16 @@
+export const metadata = {
+  title: "Santanu Konwar",
+  description: "Developer Portfolio",
+};
 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="bg-[#0b0f19] text-white">{children}</body>
+    </html>
+  );
+}
